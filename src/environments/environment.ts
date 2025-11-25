034b7db1302caf4340cfc22e97ef88243b401d02
Daniel Seguin <63018494+danieloseguin67@users.noreply.github.com>
2025-11-25 08:31:42 -0500

    change mailjs publickey and update angular17to19

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   emailjs: {
-    publicKey: 'gLAGnqUeakXx77MrF',     // Replace with your EmailJS Public Key
+    publicKey: '59pvg_Znbm41d3gqN',     // Replace with your EmailJS Public Key
     serviceId: 'service_0nh5sa8',     // Replace with your EmailJS Service ID
     templateId: 'template_as3xwbs'    // Replace with your EmailJS Template ID
   }
